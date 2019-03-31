af62b3bfbd57cf8ae8c4dc34d4620deaa4a5a074
maxiaoyanD <mxy123456>
2019-03-31 14:11:02 +0800

    massage

--- 课程练习/LS12/demo11.js
+++ 课程练习/LS12/demo11.js
@@ -12,7 +12,7 @@ function f1(){
 var f3 = f1();
 console.log(f3);//2
 console.log(f3);//2
-
+//闭包
 function f1(){
     var x = 1;
     function f2(){
@@ -23,3 +23,59 @@ function f1(){
 var f3 = f1();
 console.log(f3());//2
 console.log(f3());//3
+
+//例一
+/*
+function createInc(startValue){
+    return function(step){
+        startValue += step;
+        return startValue;
+    }
+}
+//startValue 是存储在内存中的
+var inc = createInc(5);
+console.log(inc(1));//6
+console.log(inc(2));//8
+inc = createInc(5);
+console.log(inc(1))//6
+*/
+
+//例二
+function createInc(startValue){
+	return function(step){
+		startValue+=step;
+		return startValue;
+	}
+}
+var inc = createInc(5);
+console.log(inc(1));//6
+console.log(inc(2));//8
+var inc2 = createInc(5);
+console.log(inc(1));//9
+console.log(inc2(2));//7
+
+//例三
+function foo(){
+    var i = 0;
+    function bar(){
+        console.log(++i);
+    }
+    return bar();
+}
+//注意：此处return的是bar()，是执行结果
+foo();//1
+foo();//1
+
+//例四
+function foo(){
+    var i=0;
+    function bar(){
+        console.log(++i);
+    }
+    return bar;
+}
+var a = foo();
+var b = foo();
+a();//1
+a();//2
+b();//1
